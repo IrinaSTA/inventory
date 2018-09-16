799203fec65bdf274a3863036970e576330c96e2
Irina Baldwin <2007inb@gmail.com>
2018-09-16 09:29:44 +0100

    Move tests to seperate file

--- inventory.rb
+++ inventory.rb
@@ -52,19 +52,5 @@ class Supplier
   def self.count
     @@count
   end
-  
-end
 
-apples = Product.new("granny smith appels", "Walter Clark")
-puts Product.count
-apples.name = "granny smith apples"
-puts apples.name
-apples.price = 3.5
-puts apples.price_history
-puts Supplier.count
-oranges = Product.new("blood oranges", "DHL", 4.6)
-puts Product.count
-oranges.price = 4.3
-oranges.price_history
-puts Supplier.all
-puts Supplier.count
+end
